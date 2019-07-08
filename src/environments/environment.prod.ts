b47bda744ca8a32ee2cd992b5e016f1f38a0295e
Gamaliel <gamaliel.ch.chonteco@gmail.com>
2019-07-08 12:34:45 -0500

    FireBase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  url: 'http://192.168.0.14:3000'
+  url: 'http://192.168.0.14:3000',
+  firebaseconfig: {
+    apiKey: "AIzaSyBW9S7UPjp924CU_2O42KXDRE2FX2U0BuU",
+    authDomain: "negocio-1de07.firebaseapp.com",
+    databaseURL: "https://negocio-1de07.firebaseio.com",
+    projectId: "negocio-1de07",
+    storageBucket: "",
+    messagingSenderId: "985865787580",
+    appId: "1:985865787580:web:42ce4ac1462f2e42"
+  }
 };
