53c9a572ee9be13d5f4f183a054e9c7323f4dc36
uli <ulisesmomu123@gmail.com>
2019-07-15 16:03:36 -0500

    categorias-listo

--- src/app/pages/categorias/categorias.component.ts
+++ src/app/pages/categorias/categorias.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalCategoriasComponent } from '../../components/modal-categorias/modal-categorias.component';
+import { ModalMainComponent } from '../../components/modal-main/modal-main.component';
 import { ModalController } from '@ionic/angular';
 import { CategoriasService } from '../../services/categorias.service';
+import { CrudService } from '../../services/crud.service';
+import { async } from '@angular/core/testing';
 
 @Component({
   selector: 'app-categorias',
@@ -10,112 +12,33 @@ import { CategoriasService } from '../../services/categorias.service';
 })
 export class CategoriasComponent implements OnInit {
 
-  categorias: any[];
-  value = 0;
+  categorias: any = [];
+  categoria: any = {};
 
-  constructor(private modal: ModalController, private cateService: CategoriasService) {
-    this.initItem();
-    this.categorias[0].open = true;
-   }
+  constructor(private modal: ModalController, private crudService: CrudService) { }
 
   ngOnInit() {
-  this.cateService.getAllcategorias();
-}
-
-async openModal() {
-  const modal = await this.modal.create({
-    component: ModalCategoriasComponent,
-    componentProps: {
-      custom_id: this.value
-    }
-  });
-  modal.present();
-}
-initItem() {
-  this.categorias = [
-    {
-      nombre: 'Refresco',
-      detalles: [
-        {
-          categoria: 'Bebidas',
-          descripcion: 'Refresco de cola',
-          costo: [
-            {
-              ideal: 15,
-              minimo: 10,
-              maximo: 20
-            }
-          ],
-          marca: 'Coca Cola',
-          existencia: 10,
-          precio: [
-            {
-              uno: 16,
-              dos: 18,
-              tres: 26,
-              cuatro: 36
-            }
-          ],
-          observaciones: 'Caducan mañana'
-        }
-      ]
-    },
-    {
-      nombre: 'Galletas',
-      detalles: [
-        {
-          precio: 10,
-          marca: 'Gamesa',
-          cantidad: 2
-        }
-      ]
-    },
-    {
-      nombre: 'Sabritas',
-      detalles: [
-        {
-          precio: 11,
-          marca: 'Sabritas',
-          cantidad: 15
-        }
-      ]
-    },
-    {
-      nombre: 'Jugo',
-      detalles: [
-        {
-          precio: 11.5,
-          marca: 'Jumex',
-          cantidad: 22
-        }
-      ]
-    }
-  ];
-}
-
-toggleCategoria(index) {
-  this.categorias[index].open = !this.categorias[index].open;
-
-  if (this.categorias[index].open) {
-    this.categorias
-    .filter((item, itemIndex) => itemIndex !== index)
-    .map(item => item.open = false);
+    this.crudService.init('categorias');
+    this.crudService.getAll().subscribe(resp => {
+      console.log('Obteniendo categorias->', resp);
+      this.categorias = resp;
+    });
   }
-}
 
-toggleDetalles(index, childIndex) {
-  this.categorias[index].children[0].children[childIndex].open = !this.categorias[index].children[0].children[childIndex].open;
-}
-
-getNombre(ev: any) {
-  this.initItem();
-
-  const val = ev.target.value;
-
-  if (val && val.trim() !== '') {
-      this.categorias = this.categorias.filter((item) => {
-        return (item.nombre.toLowerCase().indexOf(val.toLowerCase()) > -1);
-      });
+  async abrirModal() {
+    const modal = await this.modal.create({
+      component: ModalMainComponent,
+      componentProps: {
+        formulario: 'categorias',
+        objeto: this.categoria
+      }
+    });
+    await modal.present();
+    const { data } = await modal.onDidDismiss();
+    console.log(data);
+    if (data) {
+      this.categoria = data.objeto;
+      this.crudService.add(this.categoria);
+    }
   }
-}
 }
